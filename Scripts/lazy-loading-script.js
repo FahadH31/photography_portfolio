6cd6ec10e2bce3b871a0640e2e31c5b35c3e52e3
FahadH31 <129327022+FahadH31@users.noreply.github.com>
2024-10-18 15:22:42 -0400

    Image Loading Fix
    Fixed:
    - When navigating back to the main page from the 'About' page, non-lazy-loaded images fail to load.

--- Scripts/lazy-loading-script.js
+++ Scripts/lazy-loading-script.js
@@ -23,3 +23,16 @@ function preload_image(img) {
   for(image of images) {
     observer.observe(image);
   }
+
+  // Ensure lazy loading is re-applied when the page fully loads
+  window.addEventListener('DOMContentLoaded', (event) => {
+  let images = document.querySelectorAll('img.lazy-load');
+  for(let image of images) {
+    observer.observe(image);
+  }
+  });
+
+  // Ensure observer resets
+  window.addEventListener('beforeunload', () => {
+    observer.disconnect(); // Disconnect observer when leaving the page
+  });
